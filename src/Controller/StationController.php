beb8bfc18ccd8798524ef5db15c24cd5c57c59ad
Mahydit Anber <mahydit_anber@outlook.com>
2021-08-15 14:18:54 +0200

    add station list action

---
+++ src/Controller/StationController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\StationRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/station")
+ */
+class StationController extends AbstractController
+{
+    /**
+     * @Route("/", name="station_index", methods={"GET"})
+     */
+    public function index(StationRepository $stationRepository): Response
+    {
+        return $this->render('station/index.html.twig', [
+            'stations' => $stationRepository->findAll(),
+        ]);
+    }
+}
